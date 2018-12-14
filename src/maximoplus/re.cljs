d1eb93ac3589ca0ab5cfac3cfc45364da70afa4c
Dusan Miloradovic <dusan.m@maximoplus.com>
2018-12-14 15:26:59 +0400

    changes

--- src/maximoplus/re.cljs
+++ src/maximoplus/re.cljs
@@ -858,10 +858,13 @@
   (wf-finished
    [this]
    ;;should just close the workflow control
-   (.call (-> this (aget "wrapped") (aget "closeDialog")) (aget this "wrapped"))
+   (set-wrapped-state this (fn [state] #js{"finished" #js true}));;this will notify react component to close the wf dialog
+   (.call (-> this (aget "wrapped") (aget "closeDialog")) (aget this "wrapped"));;for the react template this will not do anything, closing will be done explicitely from the control itself
+   
    )
   (cancel-wf
    [this]
+   (set-wrapped-state this (fn [state] #js{"finished" #js true}))
    (.call (-> this (aget "wrapped") (aget "closeDialog")) (aget this "wrapped"))
    )
   (^override set-wf-title
